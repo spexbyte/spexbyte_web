576dbe7326d038aa3fd25f0dd1b2756fe6774105
bjamyl <alhassanjamil0@gmail.com>
2022-07-31 23:09:22 +0000

    added client component and styling

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Clients from "../components/HomepageComponents/Clients";
 import FeaturedWorks from "../components/HomepageComponents/FeaturedWorks";
 import Hero from "../components/HomepageComponents/Hero";
 import WhatWeDo from "../components/HomepageComponents/WWD/WhatWeDo";
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <FeaturedWorks />
       <WhatWeDo />
+      <Clients/>
     </div>
   );
 }

b14fb21b4baad929e578ca0254525ae871af2c1d
bjamyl <alhassanjamil0@gmail.com>
2022-08-09 12:37:34 +0000

    styled about components and installed tailwind scrollbar hide

--- components/AboutPageComponents/Hero.js
+++ components/AboutPageComponents/Hero.js
@@ -1,16 +1,23 @@
+import Image from "next/image";
+
 const AboutHero = () => {
   return (
     <div>
-      <h1 className="font-medium text-5xl xl:text-8xl">
+      <div className="mb-10">
+        <div className=" relative w-iw h-ih xl:w-sw xl:h-idh">
+          <Image src="/assets/team.jpg" layout="fill" alt="team photo" />
+        </div>
+      </div>
+      <h1 className="font-medium text-5xl xl:text-7xl">
         We are spexbyte<span className="text-accent">.</span>
       </h1>
-      <p className="font-light text-lg xl:text-2xl xl:w-mtw">
+      <p className="font-light text-lg xl:text-2xl xl:w-3/5">
         We are a small, passionate group of creatives who grew tired of the old,
         clunky digital experiences. No matter how crazy your idea, we are happy
         to talk through it.
       </p>
-      <div>
-        <h3 className="text-xl font-medium hover:cursor-pointer xl:text-3xl">
+      <div className="w-fit mt-4">
+        <h3 className="text-lg font-medium xl:text-xl hover:cursor-pointer">
           OUR WORKS
         </h3>
         {/* Underline */}
--- components/AboutPageComponents/ShowValues.js
+++ components/AboutPageComponents/ShowValues.js
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 const ShowValues = ({ icon, headerText, mainText }) => {
   return (
-    <div>
-      <Image src={icon} alt={headerText} width={100} height={100} />
-      <h4>{headerText}</h4>
-      <p>{mainText}</p>
+    <div className="w-auto xl:flex-shrink-0 xl:w-[1280px]">
+      <Image src={icon} alt={headerText} width={50} height={50} />
+      <h4 className="text-2xl">{headerText}</h4>
+      <p className="font-light text-lg xl:w-[600px]">{mainText}</p>
     </div>
   );
 };
